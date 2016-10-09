6bf7b545b23609f0025c03d3735ad52f06e19e51
JamesBrownlow <jbrownlow@avc.edu>
2016-10-09 10:10:19 -0700

    getting there

--- Install_Course.R
+++ Install_Course.R
@@ -1,2 +1,11 @@
 y<-read.csv(file="coursePacks.csv",header=TRUE)
-install.packages(y,dependencies = TRUE)
+install.packages(y$Package,dependencies = TRUE)
+
+"xtable" %in% rownames(installed.packages())
+
+
+a = unlist(list(y$Package))
+a
+str(a)
+a[1]
+a[100]
