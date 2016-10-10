2046b04b924c487fd05bc3281a59554815b980cf
JamesBrownlow <jbrownlow@avc.edu>
2016-10-09 17:33:05 -0700

    check for package before you load

--- Install_Course.R
+++ Install_Course.R
@@ -1,11 +1,11 @@
 y<-read.csv(file="coursePacks.csv",header=TRUE)
-install.packages(y$Package,dependencies = TRUE)
-
-"xtable" %in% rownames(installed.packages())
-
+#install.packages(y$Package,dependencies = TRUE)
 
 a = unlist(list(y$Package))
-a
-str(a)
-a[1]
-a[100]
+b=as.character(a[1])
+
+for (i in length(y)){
+  testPackage=as.character(a[i])
+  if (!(testPackage %in% rownames(installed.packages())))
+    install.packages(testPackage)
+}
