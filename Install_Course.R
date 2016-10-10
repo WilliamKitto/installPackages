15d87e4227e8ce1fc8a06fb043bbddf3c8356f89
JamesBrownlow <jbrownlow@avc.edu>
2016-10-09 17:37:40 -0700

    check, install, and update

--- Install_Course.R
+++ Install_Course.R
@@ -2,10 +2,12 @@ y<-read.csv(file="coursePacks.csv",header=TRUE)
 #install.packages(y$Package,dependencies = TRUE)
 
 a = unlist(list(y$Package))
-b=as.character(a[1])
 
 for (i in length(y)){
   testPackage=as.character(a[i])
   if (!(testPackage %in% rownames(installed.packages())))
     install.packages(testPackage)
 }
+
+#let user decide to update or not...
+update.packages()
