88a24491deb829144ba9ea9d075985798dc1c0cc
wkitto <wkitto1@avc.edu>
2016-10-30 11:51:37 -0700

    added Homers lattice and ggplot2 addins

--- Install_Course.R
+++ Install_Course.R
@@ -12,3 +12,11 @@ for (i in 1:length(a)){
 #let user decide to update or not...
 update.packages()
 
+#insert homers plot addins
+
+if(!require("devtools")) {install.packages("devtools");require("devtools")}
+
+devtools::install_github("homerhanumat/addinplots")
+devtools::install_github("homerhanumat/shinyCustom")
+devtools::install_github("homerhanumat/addinggplot2")
+
