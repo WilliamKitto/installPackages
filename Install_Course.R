a4bcda02a19e55a7f566aaac430cd47b85cedf25
imclaptop <imclaptop@BE113-72311MR.local>
2016-11-03 15:53:17 -0700

    clean swirl package install

--- Install_Course.R
+++ Install_Course.R
@@ -1,6 +1,21 @@
 y<-read.csv(file="coursePacks.csv",header=TRUE)
 #install.packages(y$Package,dependencies = TRUE)
 
+if(!require("devtools")) {install.packages("devtools");require("devtools")}
+devtools::install_github("swirldev/swirl", ref = "dev",force=TRUE)
+install_from_swirl("R_Programming", dev = FALSE, mirror = "github")
+install_from_swirl("Data_Analysis", dev = FALSE, mirror = "github")
+install_from_swirl("Exploratory_Data_Analysis", dev = FALSE, mirror = "github")
+install_from_swirl("Regression_Models", dev = FALSE, mirror = "github")
+install_from_swirl("Statistical_Inference", dev = FALSE, mirror = "github")
+
+#insert homers plot addins
+
+devtools::install_github("homerhanumat/addinplots",force = TRUE)
+devtools::install_github("homerhanumat/shinyCustom")
+devtools::install_github("homerhanumat/addinggplot2")
+
+
 a = unlist(list(y$Package))
 
 for (i in 1:length(a)){
@@ -9,14 +24,12 @@ for (i in 1:length(a)){
     install.packages(testPackage)
 }
 
-#let user decide to update or not...
-update.packages()
+#let user decide to update or not...not needed on new install
+#update.packages()
 
-#insert homers plot addins
 
-if(!require("devtools")) {install.packages("devtools");require("devtools")}
 
-devtools::install_github("homerhanumat/addinplots")
-devtools::install_github("homerhanumat/shinyCustom")
-devtools::install_github("homerhanumat/addinggplot2")
+
+
+
 
